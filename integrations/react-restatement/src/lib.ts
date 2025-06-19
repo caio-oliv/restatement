8d7949f487339799a056edbd3789f902d0d54010
Caio Oliveira <caio.vsoliveira23@gmail.com>
2025-06-19 13:50:06 -0300

    feat: setup the react integration lib

---
+++ integrations/react-restatement/src/lib.ts
@@ -0,0 +1,10 @@
+/**
+ * @description test
+ * @returns promise
+ */
+export async function lib(): Promise<void> {
+	// eslint-disable-next-line no-console
+	console.log('test');
+}
+
+lib();
---
+++ rollup.config.js
@@ -0,0 +1,75 @@
+/* eslint-disable jsdoc/check-tag-names */
+
+import { join } from 'node:path';
+import { readFileSync } from 'node:fs';
+import { cwd } from 'node:process';
+import typescript from '@rollup/plugin-typescript';
+
+// interface PackageJson {
+// 	type: 'module';
+// 	source: string;
+// 	main: string;
+// 	module: string;
+// 	types: string;
+// 	dependencies?: Record<string, string>;
+// 	devDependencies?: Record<string, string>;
+// 	peerDependencies?: Record<string, string>;
+// }
+
+/**
+ * @typedef PackageJson
+ * @type {object}
+ * @property {'module'} type -
+ * @property {string} source -
+ * @property {string} main -
+ * @property {string} module -
+ * @property {string} types -
+ * @property {Record<string, string>} dependencies -
+ * @property {Record<string, string>} devDependencies -
+ * @property {Record<string, string>} peerDependencies -
+ */
+
+const projectPath = cwd();
+
+const tsconfigPath = join(projectPath, 'tsconfig.json');
+const packageJsonPath = join(projectPath, 'package.json');
+
+/**
+ * @type {PackageJson}
+ */
+// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+const packageJson = JSON.parse(readFileSync(packageJsonPath, { encoding: 'utf-8' }));
+const peerDependencies = Object.keys(packageJson.peerDependencies ?? {});
+
+/**
+ * @type {import('rollup').RollupOptions}
+ */
+const config = {
+	input: packageJson.source,
+	external: peerDependencies,
+	plugins: [
+		typescript({
+			project: projectPath,
+			tsconfig: tsconfigPath,
+			include: ['src/**/*.ts'],
+			composite: false,
+			incremental: false,
+			declaration: false,
+			outputToFilesystem: true,
+		}),
+	],
+	output: [
+		{
+			file: packageJson.main,
+			sourcemap: true,
+			format: 'cjs',
+		},
+		{
+			file: packageJson.module,
+			sourcemap: true,
+			format: 'es',
+		},
+	],
+};
+
+export default config;
