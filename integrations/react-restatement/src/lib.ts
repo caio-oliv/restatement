afd7c515aa43097e4db5af9e72c37a4f215abb4d
Caio Oliveira <caio.vsoliveira23@gmail.com>
2025-09-06 16:13:54 -0300

    feat: add config context, provider and test helpers

--- integrations/react-restatement/src/lib.ts
+++ integrations/react-restatement/src/lib.ts
@@ -1,10 +1,2 @@
-/**
- * @description test
- * @returns promise
- */
-export async function lib(): Promise<void> {
-	// eslint-disable-next-line no-console
-	console.log('test');
-}
-
-lib();
+export * from '@/context/RestatementContext';
+export * from '@/context/RestatementProvider';
